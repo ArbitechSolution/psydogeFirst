079b721ee7f29991c4ae7d2361e35a2293b4d145
sufyan farooq <59057649+SufyanFarooq@users.noreply.github.com>
2021-11-17 18:25:50 +0500

    main net contract

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import "slick-carousel/slick/slick.css";
+ import "slick-carousel/slick/slick-theme.css";
+ import { ToastContainer } from 'react-toastify';
+
+function App() {
+  return (
+    <div>
+      <ToastContainer />
+      <Header />
+      <Hero />
+
+    </div>
+  );
+}
+
+export default App;
